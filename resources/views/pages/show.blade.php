2beb8d9f486c0daa38428572120d5c0b315ba352
Timjini <hatim.jini@gmail.com>
2025-08-20 22:52:49 +0200

    image place holder

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,7 +1,5 @@
 
 <x-app-layout>
-  <img src="{{ route('pages.image', basename($page->image_url)) }}" alt="{{ $page->title }}">
-
 
     @include('pages.partials.header',['pageTitle' => $page->section->name])
      <!-- Breadcrumbs -->
@@ -19,12 +17,19 @@
             @include('pages.partials.aside', ['pages'=> $page->category->pages])
             
             <main class="lg:w-3/4">
-            <!-- Page Header -->
-            <div class="mb-8">
-                <h1 class="text-3xl font-bold text-bce-steel mb-2">{{$page->title}}</h1>
-                <div class="h-1 w-20 bg-bce-accent mb-4"></div>
-                <p class="text-lg text-gray-700"></p>
-            </div>
+                <div class="relative w-full h-96 bg-cover bg-center rounded-xl shadow-md bg-[url('{{ $page->image_url ? route('pages.image', basename($page->image_url)) : asset('images/place_holder.png') }}')]"
+                    alt="{{ $page->title }}')">
+                    <!-- Dark overlay -->
+                    <div class="absolute inset-0 bg-black bg-opacity-40 rounded-xl"></div>
+
+                    <!-- Title on top -->
+                    <div class="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
+                        <h1 class="text-4xl md:text-5xl font-bold drop-shadow-lg">{{ $page->title }}</h1>
+                        <div class="h-1 w-20 bg-bce-accent my-4"></div>
+                        <p class="text-lg">Your subtitle or description here</p>
+                    </div>
+                </div>
+
             {!! $page->content !!}
             
             @include('pages.partials.action')
